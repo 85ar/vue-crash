7f72e81b1d4fcab1e41d359b72cf1e4e92479589
Artur Valeev <85ar@rambler.ru>
2023-07-12 23:48:15 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <h1>Todo list</h1>
+    <AddTodo @add-todo="addTodo" />
+    <hr />
+    <TodoList v-bind:todos="todos" @remove-todo="removeTodo" />
+  </div>
+</template>
+
+<script>
+import TodoList from "./components/TodoList.vue";
+import AddTodo from "./components/AddTodo.vue";
+export default {
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "Купить хлеб",
+          complited: false,
+        },
+        {
+          id: 2,
+          title: "Купить масло",
+          complited: false,
+        },
+        {
+          id: 3,
+          title: "Купить сыр",
+          complited: false,
+        },
+      ],
+    };
+  },
+  components: {
+    TodoList,
+    AddTodo,
+  },
+  methods: {
+    removeTodo(id) {
+      this.todos = this.todos.filter((t) => t.id !== id);
+    },
+    addTodo(todo) {
+      this.todos.push(todo);
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,41 @@
+<template>
+  <li>
+    <span v-bind:class="{ done: todo.complited }">
+      <input type="checkbox" v-on:change="todo.complited = !todo.complited"/>
+      <strong>{{ todo.id }}</strong>
+      {{ todo.title }}
+    </span>
+    <button v-on:click="$emit('remove-todo', todo.id)" class="remove">&times;</button>
+  </li>
+</template>
+<script>
+export default {
+  props: {
+    todo: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+<style scoped>
+li {
+  border: 1px solid #ccc;
+  display: flex;
+  justify-content: space-between;
+  padding: 0.5rem 2rem;
+  margin-bottom: 1rem;
+}
+input {
+  margin-right: 1rem;
+}
+.remove {
+  background: red;
+  color: #fff;
+  border-radius: 50%;
+  font-weight: bold;
+}
+.done {
+  text-decoration: line-through;
+}
+</style>
